6e40364a6316417ac3c37e5c4fe1cdb7454109f8
wendyyval <wval0700@gmail.com>
2025-10-12 19:24:55 -0500

    render deploy check

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,6 +1,7 @@
 import {useState} from "react";
 import {useNavigate} from "react-router-dom";
 import AuthLayout from "../AuthLayout";
+import { apiFetch } from "../utils/http";
 
 
 
@@ -22,7 +23,7 @@ export default function Login(){
 
     setLoading(true);
     try {
-        const res = await fetch("/api/auth/login", {
+        const res = await apiFetch("/api/auth/login", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ email, password }),
