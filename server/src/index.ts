1c12411a968a16ccb381a710e968e0854b105f10
wendyyval <wval0700@gmail.com>
2025-10-13 13:12:24 -0500

    changes to package.json server

--- server/src/index.ts
+++ server/src/index.ts
@@ -3,5 +3,5 @@ import app from "./app";
 const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, () => {
-  console.log(`API running on http://localhost:${PORT}`);
+  console.log(`API running on :${PORT}`);
 });
