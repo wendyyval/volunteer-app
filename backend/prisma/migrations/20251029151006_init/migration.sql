a685919b19b49337d64361a904939e53ecdfaa33
Wendy Valdez <wval0700@gmail.com>
2025-10-29 10:27:29 -0500

    added test seed to verify if data was being sent to database

---
+++ backend/prisma/migrations/20251029151006_init/migration.sql
@@ -0,0 +1,114 @@
+-- CreateTable
+CREATE TABLE `user_credentials` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `email` VARCHAR(191) NOT NULL,
+    `password_hash` VARCHAR(191) NOT NULL,
+    `role` VARCHAR(191) NOT NULL,
+    `created_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updated_at` DATETIME(3) NOT NULL,
+
+    UNIQUE INDEX `user_credentials_email_key`(`email`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `user_profile` (
+    `user_id` INTEGER NOT NULL,
+    `full_name` VARCHAR(191) NOT NULL,
+    `address1` VARCHAR(191) NOT NULL,
+    `address2` VARCHAR(191) NULL,
+    `city` VARCHAR(191) NOT NULL,
+    `state` VARCHAR(191) NOT NULL,
+    `zip` VARCHAR(191) NOT NULL,
+    `preferences` VARCHAR(191) NULL,
+
+    PRIMARY KEY (`user_id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `skills` (
+    `skill_id` INTEGER NOT NULL AUTO_INCREMENT,
+    `skill_name` VARCHAR(191) NOT NULL,
+
+    UNIQUE INDEX `skills_skill_name_key`(`skill_name`),
+    PRIMARY KEY (`skill_id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `user_skills` (
+    `user_id` INTEGER NOT NULL,
+    `skill_id` INTEGER NOT NULL,
+
+    PRIMARY KEY (`user_id`, `skill_id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `availability` (
+    `user_id` INTEGER NOT NULL,
+    `available_date` DATETIME(3) NOT NULL,
+
+    PRIMARY KEY (`user_id`, `available_date`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `event_details` (
+    `event_id` INTEGER NOT NULL AUTO_INCREMENT,
+    `event_name` VARCHAR(191) NOT NULL,
+    `description` VARCHAR(191) NOT NULL,
+    `location` VARCHAR(191) NOT NULL,
+    `urgency` ENUM('Low', 'Medium', 'High') NOT NULL,
+    `event_date` DATETIME(3) NOT NULL,
+
+    PRIMARY KEY (`event_id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `event_skills` (
+    `event_id` INTEGER NOT NULL,
+    `skill_id` INTEGER NOT NULL,
+
+    PRIMARY KEY (`event_id`, `skill_id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `volunteer_history` (
+    `history_id` INTEGER NOT NULL AUTO_INCREMENT,
+    `user_id` INTEGER NOT NULL,
+    `event_id` INTEGER NOT NULL,
+    `participation_date` DATETIME(3) NOT NULL,
+    `status` ENUM('Registered', 'Confirmed', 'Attended', 'No_Show', 'Canceled', 'Withdrawn') NOT NULL,
+
+    PRIMARY KEY (`history_id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `States` (
+    `code` VARCHAR(191) NOT NULL,
+    `name` VARCHAR(191) NOT NULL,
+
+    PRIMARY KEY (`code`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `user_profile` ADD CONSTRAINT `user_profile_user_id_fkey` FOREIGN KEY (`user_id`) REFERENCES `user_credentials`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `user_skills` ADD CONSTRAINT `user_skills_user_id_fkey` FOREIGN KEY (`user_id`) REFERENCES `user_profile`(`user_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `user_skills` ADD CONSTRAINT `user_skills_skill_id_fkey` FOREIGN KEY (`skill_id`) REFERENCES `skills`(`skill_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `availability` ADD CONSTRAINT `availability_user_id_fkey` FOREIGN KEY (`user_id`) REFERENCES `user_profile`(`user_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `event_skills` ADD CONSTRAINT `event_skills_event_id_fkey` FOREIGN KEY (`event_id`) REFERENCES `event_details`(`event_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `event_skills` ADD CONSTRAINT `event_skills_skill_id_fkey` FOREIGN KEY (`skill_id`) REFERENCES `skills`(`skill_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `volunteer_history` ADD CONSTRAINT `volunteer_history_user_id_fkey` FOREIGN KEY (`user_id`) REFERENCES `user_credentials`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `volunteer_history` ADD CONSTRAINT `volunteer_history_event_id_fkey` FOREIGN KEY (`event_id`) REFERENCES `event_details`(`event_id`) ON DELETE CASCADE ON UPDATE CASCADE;
